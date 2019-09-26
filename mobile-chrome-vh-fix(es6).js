ca2f70d3def6a820f9de2de1b5fdfcc1b53e790f
kiuKisas <martkinoo@gmail.com>
2019-09-26 11:18:55 +0200

    replace for loop by forEach for clariy + linter stuff

--- mobile-chrome-vh-fix(es6).js
+++ mobile-chrome-vh-fix(es6).js
@@ -1,72 +1,65 @@
 export default class VHChromeFix {
-
   constructor(selectors) {
+    this.selectors = selectors
+    let userAgent = navigator.userAgent.toLowerCase()
+    this.isAndroidChrome = /chrome/.test(userAgent) && /android/.test(userAgent)
+    this.isIOSChrome = /crios/.test(userAgent)
 
-    this.selectors = selectors;
-    let userAgent = navigator.userAgent.toLowerCase();
-    this.isAndroidChrome = /chrome/.test(userAgent) && /android/.test(userAgent);
-    this.isIOSChrome = /crios/.test(userAgent);
-
-    this.init();
+    this.init()
   }
 
   init() {
-
-    let self = this;
+    let self = this
 
     if (self.isAndroidChrome || self.isIOSChrome) {
       // If we detected Chrome on Android or iOS
       // Cache elements and trigger fix on init
-      self.getElements(self.selectors);
-      self.fixAll();
+      self.getElements(self.selectors)
+      self.fixAll()
 
       // Cache window dimensions
-      self.windowWidth = window.innerWidth;
-      self.windowHeight = window.innerHeight;
+      self.windowWidth = window.innerWidth
+      self.windowHeight = window.innerHeight
 
-      window.addEventListener('resize', function () {
+      window.addEventListener('resize', function() {
         // Both width and height changed (orientation change)
         // This is a hack, as Android when keyboard pops up
         // Triggers orientation change
-        self.windowWidth = window.innerWidth;
-        self.windowHeight = window.innerHeight;
-        self.fixAll();
-      });
+        self.windowWidth = window.innerWidth
+        self.windowHeight = window.innerHeight
+        self.fixAll()
+      })
     }
-
   }
 
   getElements(selectors) {
-    let self = this;
+    let self = this
 
-    self.elements = [];
+    self.elements = []
     // Convert selectors to array if they are not
-    selectors = Array.isArray(selectors) ? selectors : [selectors];
+    selectors = Array.isArray(selectors) ? selectors : [selectors]
 
-    for (let i = 0; i < selectors.length; i++) {
+    selectors.forEach(data => {
       // Get all elements for selector
-      let selector = selectors[i].selector;
-      let elements = document.querySelectorAll(selector);
-
+      const elements = document.querySelectorAll(data.selector)
       // Go through all elements for one selector to filter them
-      for (let j = 0; j < elements.length; j++) {
-        let offset = selectors[i].offset || 0;
+      elements.forEach(element => {
+        const offset = data.offset || 0
         self.elements.push({
-          domElement: elements[j],
-          vh: selectors[i].vh,
+          domElement: element,
+          vh: data.vh,
           offset: offset
-        });
-      }
-    }
+        })
+      })
+    })
   }
 
   fixAll() {
-    let self = this;
-
-    for (let i = 0; i < self.elements.length; i++) {
-      let element = self.elements[i];
-      let offset = element.offset;
-      element.domElement.style.height = (window.innerHeight * (element.vh / 100) - offset) + 'px';
-    }
-  };
+    let self = this
+    self.elements.forEach(element => {
+      const size =
+        window.innerHeight * (element.vh / 100) - element.offset + 'px'
+      element.domElement.style.height = size
+    })
+  }
 }
